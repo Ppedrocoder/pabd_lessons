4061fc1e3254ec5d4706b37d3ced4b64267d0c97
Pedro <rpedroricardo87@gmail.com>
2026-05-05 13:44:47 -0300

    feat: add project, settings and starting models

---
+++ copa/copa/torneio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Grupo(models.Model):
+    nome = models.CharField(max_length=1)
+    descricao = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f'Grupo {self.nome}'
+
+class Tecnico(models.Model):
+    nome = models.CharField(max_length=150)
+    nacionalidade = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+class Selecao(models.Model):
+    nome = models.CharField(max_length=100)
+    sigla = models.CharField(max_length=3, unique=True)
+    confederacao = models.CharField(max_length=50)
+    grupo = models.ForeignKey(Grupo, on_delete=models.PROTECT, related_name='selecoes')
+    tecnico = models.OneToOneField(Tecnico, on_delete=models.SET_NULL, null=True, blank=True, related_name='selecao')
+    escudo_url = models.URLField(blank=True, null=True)
